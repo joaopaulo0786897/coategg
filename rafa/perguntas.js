16bc16abaa7bf7a275e82021055642e492da816d
joaopaulo0786897 <162650657+joaopaulo0786897@users.noreply.github.com>
2024-10-17 22:44:48 -0300

    Add files via upload

---
+++ rafa/perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'assuntos gerais',
+    'qual o motor do skyline r34?',
+    'seu motor é o RB26 TWIN TURBO',
+
+)
+criaCartao(
+    'Sobre filmes',
+    'quantas temporadas tem o seriado the walking dead?',
+    'o seriado contém 11 temporadas',
+
+)
+criaCartao(
+    'geografia',
+    'qual o maior pais do mundo?',
+    'o maior pais do mundo é a Russia',
+
+)
+criaCartao(
+    'sobre filmes',
+    'em qual filme de velozes e furiosos tem a aparição do icõnico toyota supra?',
+    'ele aparece no primeiro filme da franquia sendo assim no ano de 2001',
+    
+)
